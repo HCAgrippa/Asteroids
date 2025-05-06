1a18f8807f4540afc0d77daa28c235fa78fa1873
chris <kreklewetz@gmail.com>
2025-05-06 06:00:29 -0400

    movement

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
@@ -17,14 +20,18 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+            
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
         
         # limit framerate to 60 FPS
         dt = clock.tick(60) / 1000
-
+        
 
 if __name__ == "__main__":
     main()
